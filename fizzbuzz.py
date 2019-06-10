155f7458ad5b6a388ed1afdcc5481190c5746540
heunsunyoon <heunsunyoon@gmail.comclearq>
2019-06-11 01:25:47 +0900

    list comprehension_step01

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,18 +5,25 @@
 # 15의 배수, "fizzbuzz"
 # 나머지, 그 숫자를 출력하시오
 
+#value = int(input("insert number:"))
+
+#for i in range(1, value+1):
+
+#    if i % 15 == 0:
+#        print("fizzbuzz")
+	
+#    elif i % 3 == 0:
+#        print("fizz")
+	
+#    elif i % 5 == 0:
+#        print("buzz")
+	
+#    else:
+#        print("i")
+
 value = int(input("insert number:"))
-
-for i in range(1, value+1):
-
-    if i % 15 == 0:
-        print("fizzbuzz")
-	
-    elif i % 3 == 0:
-        print("fizz")
-	
-    elif i % 5 == 0:
-        print("buzz")
-	
-    else:
-        print("i")
+result = [
+    str(num) + ":fizzbuzz" if num % 15 == 0 else str(num) + ":i"
+    for num in range(1, value+1)
+]
+print(result)
